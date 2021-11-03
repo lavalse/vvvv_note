81e5487c7be5e93d088108772bb166b13befcd4c
XU Cong <lavalselike@gmail.com>
2021-11-03 16:42:21 +0900

    add new study

---
+++ docs/Fuse/swizzle.md
@@ -0,0 +1,12 @@
+---
+sidebar_position: 1
+---
+
+# Swizzle
+
+对于GPU编程以及同时又想要可视化编程，一个重要的点是如何获取一个vector中的单个通道的数据（x,y,z,w也好，或者是r,g,b,a也好）。
+
+因而fuse提供了可以操作单个通道的一系列节点：
+
+![swizzle](.\img\swizzle.png)
+
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Uncle RED',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -53,9 +53,9 @@ const config = {
             type: 'doc',
             docId: 'intro',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Docs',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          // {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
